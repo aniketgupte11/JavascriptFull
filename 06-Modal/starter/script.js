7c8ee50c178a6ce85c6aaaa316f770e9f012d6a1
Aniket Gupte <aniketgupte11@gmail.com>
2026-06-24 17:46:03 +0530

    Modal Code complete

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -1,1 +1,35 @@
 'use strict';
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+const btnShowModal = document.querySelectorAll('.show-modal'); // selects all occurences
+console.log(btnShowModal);
+
+const openModal = () => {
+  console.log('Button clicked');
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+const closeModal = () => {
+  console.log('Close Button clicked');
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+for (let i = 0; i < btnShowModal.length; i++) {
+  console.log(btnShowModal[i].textContent);
+  btnShowModal[i].addEventListener('click', openModal);
+}
+btnCloseModal.addEventListener('click', function () {
+  closeModal();
+}); // adding a callback and then giving the function for execution
+overlay.addEventListener('click', closeModal);
+
+document.addEventListener('keydown', function (e) {
+  console.log(e.key);
+  if (e.key === 'Escape') {
+    if (!modal.classList.contains('hidden')) {
+      closeModal();
+    }
+  }
+});
